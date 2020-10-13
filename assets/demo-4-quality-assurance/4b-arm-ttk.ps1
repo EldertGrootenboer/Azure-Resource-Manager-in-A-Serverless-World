6f3a5c025053a9c9e436da4d06bc1d67b153ca0a
Eldert Grootenboer <eldert@eldert.net>
2020-10-13 13:39:54 +0200

    Some updates from dry-run

--- assets/demo-4-quality-assurance/4b-arm-ttk.ps1
+++ assets/demo-4-quality-assurance/4b-arm-ttk.ps1
@@ -1,5 +1,5 @@
 # What we will be doing in this script.
-#   1. Update ./Azure-Resource-Manager-in-A-Serverless-World/assets/arm-ttk/arm-ttk/testcases/deploymentTemplate/Location-Should-Not-Be-Hardcoded.test.ps1 to custom test
+#   1. Update ./assets/arm-ttk/arm-ttk/testcases/deploymentTemplate/Location-Should-Not-Be-Hardcoded.test.ps1 to custom test
 #   2. Use ARM-TTK to check our template for best practices
 
 $basePath = "C:\Users\elder\OneDrive\Sessions\Azure-Resource-Manager-in-A-Serverless-World"
